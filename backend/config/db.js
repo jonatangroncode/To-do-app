002a9fbd9901e5d6b83c6ad8482bde00e68abce9
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-11-28 20:55:33 +0100

    feat: Server and mongodb connection
    -db.js using mongoose to connect to the mongodb cluster
    -server.js setup server to listen on specified port
    -setup Express app with MongoDB connection

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error.message)
+    throw new Error('Failed to connect to MongoDB')
+  }
+}
+
+module.exports = connectDB
