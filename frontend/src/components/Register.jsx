83e1230f94770882a06c141b4c23565eba6f6907
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-12-03 00:20:34 +0100

    feat: css for pages

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./css/Register.css"; 
+
 
 const Register = () => {
   const [username, setUsername] = useState("");
--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import './css/Todo.css'
+
 
 const Todo = () => {
   const [todos, setTodos] = useState([]);
